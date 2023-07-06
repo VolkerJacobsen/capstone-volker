e636431c2f54735e52d8518af9de92e2d5f67222
Volker Jacobsen <volkerjacobsen@gmail.com>
2023-07-06 17:53:30 +0200

    added image and data.js

---
+++ utils/data.js
@@ -0,0 +1,207 @@
+export const Projects = [
+  {
+    id: "1",
+    slug: "park-cleaning",
+    title: "Park cleaning",
+    description_short:
+      "Who said that cleaning can’t be a fun activity? Help us to keep our urban spaces in shape. ",
+    description_long:
+      "wirBerlin is a non-profit initiative that promotes sustainable urban development. Together with the Berliners, she wants to make parks, playgrounds and other public spaces cleaner and thus more livable for everyone. Among other things, the initiative fights against littering: it provides information about the problem and has developed a park etiquette. The activists are also targeting people in parks, reminding them not to leave their rubbish there.",
+    category: "Environment",
+    organizer: "wirBerlin gGmbH",
+    contact: "info@wir-berlin.org",
+    imageSource: "../assets/images/park-cleaning.jpg",
+  },
+  {
+    id: "2",
+    slug: "tree-planting",
+    title: "Tree planting",
+    description_short:
+      "Let us fight climate change together and plant some trees in our beloved wetland of the Bucher Heide.",
+    description_long:
+      "In the Bucher Forst, we have planted and cared for many trees in recent years, brought people together and made an important contribution to climate protection. Without you none of this would have been possible. Be part of the next action.",
+    category: "Environment",
+    organizer: "aufBuchen e.V.",
+    contact: "info@aufbuchen.de",
+    imageSource: "../assets/images/tree-planting.jpg",
+  },
+  {
+    id: "3",
+    slug: "anti-bias",
+    title: "Anti Bias ambassador",
+    description_short:
+      "Anti-Bias Ambassador (political education for educational professionals)",
+    description_long:
+      "We all have prejudices and biases. Because these are learned, we can question them accordingly and actively „un-learn“ them. Our aim is to inspire and to empower people of all ages to challenge preconceptions in themselves, others and society. We are looking for motivated anti-bias ambassadors who spread awareness in public institutions and corporations.",
+    category: "Politics",
+    organizer: "Paritätische Akademie Berlin GmbH",
+    contact: "paritaetische@akademie.org",
+    imageSource: "../assets/images/anti-bias.jpg",
+  },
+  {
+    id: "4",
+    slug: "piano-playing",
+    title: "Piano playing",
+    description_short: "Playing the piano with elderly people in need of care",
+    description_long:
+      "The facility is looking for dedicated volunteers who will play music or the residents or with the residents. There are two pianos The residents are very happy about musical entertainment. ",
+    category: "Community",
+    organizer: "Seniorenheim Albestraße GmbH",
+    contact: "schilde@ev-seniorenheim.de",
+    imageSource: "../assets/images/piano-playing.jpg",
+  },
+  {
+    id: "5",
+    slug: "voluntary-reading",
+    title: "Voluntary reading",
+    description_short: "Voluntary reading in public institutions",
+    description_long:
+      "As a reader, you meet with other readers once a week for an hour to look at picture books in public facilities such as libraries, schools, kindergartens with children, to read from children’s books or, if the children can already read, to listen to the children being read to them. The main aim is to awaken the children’s joy of reading.",
+    category: "Communtiy",
+    organizer: "Stiftung Gute Tat Berlin",
+    contact: "berlin@gute-tat.de",
+    imageSource: "../assets/images/reading-public.jpg",
+  },
+  {
+    id: "6",
+    slug: "relief-supply",
+    title: "Relief supply",
+    description_short:
+      "Packing and sorting of relief supplies for refugee camps",
+    description_long:
+      "We need help sorting and packing donated relief supplies for Bosnia and Greece. Our warehouse is in Biesenthal, near Bernau, 30 minutes by train from Berlin Ostkreuz. ",
+    category: "Community",
+    organizer: "Wir packen´s an e.V.",
+    contact: "axel.grafmanns@wir-packens-an.info",
+    imageSource: "../assets/images/packing-relief-supply.jpg",
+  },
+  {
+    id: "7",
+    slug: "online-counseling",
+    title: "Online counseling",
+    description_short: "Online counseling for young people in crisis",
+    description_long:
+      "Online counseling for young people in crisis and at risk of suicide. The volunteers offer anonymous, free advice and support - from young people for young people. We are looking for volunteers between the ages of 16 and 25. Volunteering is closely supported by our full-time employees",
+    category: "Community",
+    organizer: "Stiftung Gute Tat Berlin",
+    contact: "berlin@gute-tat.de",
+    imageSource: "../assets/images/online-counseling.jpg",
+  },
+  {
+    id: "8",
+    slug: "repair-cafe",
+    title: "Repair Café",
+    description_short:
+      "The RepairCafé is looking for volunteers that support us in organization and public relations.",
+    description_long:
+      "For the past three years, a lively group has been meeting to enjoy handicrafts once a month in the RepairCafé to encourage local residents help to breathe new life into broken everyday objects. With it resources are conserved and money is saved. We are looking for engaged folks to help us in the management, welcoming and registering guests and in the distribution on tools.",
+    category: "Community",
+    organizer: "RepairCafe GmbH",
+    contact: "info@repaircafe.de",
+    imageSource: "../assets/images/repair-cafe.jpg",
+  },
+  {
+    id: "9",
+    slug: "tutoring-children",
+    title: "Tutoring children",
+    description_short:
+      "We are looking for sponsors who help us to integrate young children with a migration background",
+    description_long:
+      "We arrange 1:1 sponsorships between volunteers and socially disadvantaged students with a migration background. The aim is to improve the children’s educational opportunities and create opportunities for encounters. As a godparent, you give tuition to your godchild’s family at home once a week.",
+    category: "Community",
+    organizer: "Tamaja Soziale Dienstleistungen e.V.",
+    contact: "info@tamaja.de",
+    imageSource: "../assets/images/tutoring-children.jpg",
+  },
+  {
+    id: "10",
+    slug: "mentoring-children",
+    title: "Mentoring refugees",
+    description_short:
+      "Tamaja Soziale Dienstleistungen is looking for volunteer mentors for refugees.",
+    description_long:
+      "Volunteer mentors support refugees and their families in day-to-day matters such as looking for work, housing, etc. or when dealing with authorities. In addition, as native speakers and familiar with the area, they help with orientation in a foreign culture and language. As part of the mentoring program, the mentors are placed, qualified and continuously supervised.",
+    category: "Community",
+    organizer: "Tamaja Soziale Dienstleistungen e.V.",
+    contact: "info@tamaja.de",
+    imageSource: "../assets/images/mentoring-refugees.jpg",
+  },
+  {
+    id: "11",
+    slug: "plant-protection",
+    title: "Plant protection",
+    description_short:
+      "Join our „Urbanity and Diversity“ team and help us protect wild plants",
+    description_long:
+      "The „Urbanity and Diversity“ team invites Berliners to give rare and endangered wild plants a home in their gardens, on their balconies and on their windowsills. The scientists leading the project collect wild plants that are native to Berlin and Brandenburg and propagate them in the Des and greenhouses of the University of Potsdam and the Humboldt University. They then pass the plants on to interested parties who take them home and tend to them.",
+    category: "Environment",
+    organizer: "Urbanity and Diversity e.V.",
+    contact: "info@urbanity-diversity.de",
+    imageSource: "../assets/images/urbanity-diversity.jpg",
+  },
+  {
+    id: "12",
+    slug: "fair-agriculture",
+    title: "Fair global agriculture",
+    description_short:
+      "Our aim is making global agriculture tangible. Help us to develop case studies on our 2000 m2 Weltacker. ",
+    description_long:
+      "The Weltacker in Pankow is an agricultural project that shows how arable land is used worldwide and for much agricultural land is consumed by our consumption. With their project, the Weltacker team would like to giver Berliners a feeling for this and make agriculture tangible. We have a lot of work for volunteers: We are looking for field helpers who want to help themselves, volunteers for guided tours and information events, helpers for office work, translators and many more.",
+    category: "Environment",
+    organizer: "Weltacker Pankow e.v.",
+    contact: "info@weltacker.de",
+    imageSource: "../assets/images/weltacker.jpg",
+  },
+  {
+    id: "13",
+    slug: "environmental-protection",
+    title: "Environmental protection",
+    description_short:
+      "Grüne Liga Berlin e.V. stands for all facets of environmental protection. Join us as a volunteer",
+    description_long:
+      "The Grüne Liga Berlin is an environmental association that coordinates green initiatives in Berlin and organizes its own projects. It’s about sustainability and environmental protection in all its facets. Among other things, the Grüne Liga organizes the weekly organic market at Kollwitzplatz in Prenzlauer Berg. Could you imagine helping out to organize the Grüne Liga festival, cultivating rare wild plants or taking part in another green environmental initiative in Berlin?",
+    category: "Environment",
+    organizer: "Grüne Liga e.V",
+    contact: "info@gruene-liga.de",
+    imageSource: "../assets/images/gruene-liga.jpg",
+  },
+  {
+    id: "14",
+    slug: "voters-motivation",
+    title: "Voters motivation",
+    description_short:
+      "Help us on our goal to motivate migrant first time voters to participate in the upcoming state elections",
+    description_long:
+      "By the time of the next state elections, more than 30,000 refugees from the last nine years have received German citizenship. The „Viel Respekt Stiftung“ would like to motivate those first-time voters to actively participate in politics. Hereby we would like to close an important gap in political education, because until now Germans with a migration background have hardly been addressed. ",
+    category: "Politics",
+    organizer: "Viel Respekt Stiftung e.V.",
+    contact: "info@viel-respekt.de",
+    imageSource: "../assets/images/diversity-votes.jpg",
+  },
+  {
+    id: "15",
+    slug: "political-awarness",
+    title: "Political awareness",
+    description_short:
+      "Become an ambassador for the democratic Voice of Youth e.V. in order to spread political awareness among young people.",
+    description_long:
+      "The association forms a platform for young people to network and become politically active. One goal is the creation of new ideas and their dissemination. For example through the „you move - your vision“ campaign, the youth congress, demonstrations, workshops and lectures. We are looking for engaged young people who would like to join our team and spread awareness. ",
+    category: "Politics",
+    organizer: "Voice of Youth e.V.",
+    contact: "info@voice-of-youth.de",
+    imageSource: "../assets/images/democratic-youth.jpg",
+  },
+  {
+    id: "16",
+    slug: "social-change",
+    title: "Social change",
+    description_short:
+      "Join our Street College team in order to fight racism and to create a just and equal society",
+    description_long:
+      "In cooperation with stakeholders from politics, science and representatives of the labor market, Street College is committed to initiating sustainable social change towards a culture of recognition. Our work aims to strengthen an enlightened and resilient youth movement that works for equality, justice and against racism and deprivation.",
+    category: "Politics",
+    organizer: "Street Collage e.V.",
+    contact: "info@street-collage.com",
+    imageSource: "../assets/images/street-collage.jpg",
+  },
+];
