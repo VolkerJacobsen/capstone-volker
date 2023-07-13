4ef7fe247cd7ff9a49e4c6212e0dac9b5962a210
Volker Jacobsen <volkerjacobsen@gmail.com>
2023-07-13 13:04:45 +0200

    corrected props in ProjectPreview

--- components/ProjectPreview/ProjectPreview.js
+++ components/ProjectPreview/ProjectPreview.js
@@ -18,23 +18,31 @@ const StyledImage = styled(Image)`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: black;
 `;
 
-export default function ProjectPreview({ project }) {
+export default function ProjectPreview({
+  category,
+  title,
+  imageSource,
+  organizer,
+  width,
+  height,
+  shortDescription,
+  slug,
+}) {
   return (
-    <StyledLink href={`/projects/${project.slug}`}>
+    <Link href={`/projects/${slug}`}>
       <ProjectContainer>
-        <p>{project.category}</p>
-        <h2>{project.title}</h2>
+        <p>{category}</p>
+        <h2>{title}</h2>
         <StyledImage
-          src={`/assets/images/${project.imageSource}`}
-          alt={`Photo ${project.title} by ${project.organizer}`}
-          width={project.width}
-          height={project.height}
+          src={`/assets/images/${imageSource}`}
+          alt={`Photo ${title} by ${organizer}`}
+          width={width}
+          height={height}
         ></StyledImage>
-        <p>{project.shortDescription}</p>
+        <p>{shortDescription}</p>
       </ProjectContainer>
-    </StyledLink>
+    </Link>
   );
 }
