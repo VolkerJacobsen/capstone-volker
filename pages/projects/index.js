08d9497a5f083af9b62b5f776d87cf9f640ffd8e
VolkerJacobsen <130739212+VolkerJacobsen@users.noreply.github.com>
2023-07-29 14:17:52 +0200

    Update index.js

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,17 +1,5 @@
 import ProjectList from "../../components/ProjectList/ProjectList";
-import { getAllProjects } from "../../utils/api";
 
 export default function ProjectsPage({ projects }) {
   return <ProjectList projects={projects} />;
-}
-
-export async function getStaticProps() {
-  // Fetch all projects data using the defined getAllProjects function
-  const projects = await getAllProjects();
-
-  return {
-    props: {
-      projects,
-    },
-  };
 }
